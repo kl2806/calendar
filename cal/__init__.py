4fbcd44e89335ff5b8ce935164c61cfbc4159a64
Alan Du <alanhdu@gmail.com>
2015-02-13 17:24:22 -0500

    Send events to templates

--- cal/__init__.py
+++ cal/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, render_template
-from schema import db, Event, User
-from cal.fb import update_fb_events
+from os import path
+
+from flask import Flask, g, jsonify, render_template, json, request
+from schema import db, Event
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -29,7 +30,7 @@ def page_not_found(e):
 def home():
     events = Event.query.order_by(Event.start).all()
 
-    '''sunday_events = [event for event in events if event.start.weekday() == 0]
+    sunday_events = [event for event in events if event.start.weekday() == 0]
     monday_events = [event for event in events if event.start.weekday() == 1]
     tuesday_events = [event for event in events if event.start.weekday() == 2]
     wednesday_events = [event for event in events if event.start.weekday() == 3]
@@ -37,9 +38,8 @@ def home():
     friday_events = [event for event in events if event.start.weekday() == 5]
     saturday_events = [event for event in events if event.start.weekday() == 6]
 
-    events = [sunday_events, monday_events, tuesday_events, wednesday_events, thursday_events, friday_events, saturday_events]'''
+    events = [sunday_events, monday_events, tuesday_events, wednesday_events, thursday_events, friday_events, saturday_events]
 
-    #return render_template('index.html', events=events, monday_events=monday_events, tuesday_events=tuesday_events, wednesday_events=wednesday_events, thursday_events=thursday_events, friday_events=friday_events, saturday_events=saturday_events, sunday_events=sunday_events)
     return render_template('index.html', events=events)
 
 @app.route('/update')
